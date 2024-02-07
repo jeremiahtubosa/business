804020b87977dce1ab1ddaa36fc55f81f2a4b28e
Jeremiah Tubosa <144658937+jeremiahtubosa@users.noreply.github.com>
2024-02-07 16:53:37 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript function to open the sliding box
+function openSlidingBox() {
+    document.querySelector('.sliding-box').classList.add('open');
+}
+
+// JavaScript function to close the sliding box
+function closeSlidingBox() {
+    document.querySelector('.sliding-box').classList.remove('open');
+}
